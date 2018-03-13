457bed6578cabf384c72b18c14fe5bc5d7e4b16f
PepperXu <XPS83535135@gmail.com>
2018-03-13 15:48:27 +0800

    fix index page

--- tomcat/webapps/sgTravel/WEB-INF/classes/PlanDetailsServlet.java
+++ tomcat/webapps/sgTravel/WEB-INF/classes/PlanDetailsServlet.java
@@ -26,19 +26,20 @@ public class PlanDetailsServlet extends HttpServlet {  // JDK 6 and above only
 
          // Step 2: Create a "Statement" object inside the "Connection"
          stmt = conn.createStatement();
-         out.println("<html><head><title>Plan Details</title></head><body>");
+         out.println("<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'><title>Yet Another Travel Advisor!</title><meta content='width=device-width, initial-scale=1.0' name='viewport'> <meta content='' name='keywords'> <meta content='' name='description'><!-- For-Mobile-Apps --><meta name='viewport' content='width=device-width, initial-scale=1' /><meta http-equiv='Content-Type' content='text/html; charset=utf-8' /><meta name='keywords' content='Travel Packages Widget Responsive, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design' /><script type='application/x-javascript'> addEventListener('load', function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script><!-- //For-Mobile-Apps --><!-- Style --><link rel='stylesheet' href='css/style.css' type='text/css' media='all' /><link href='css/font-awesome.css' rel='stylesheet'><!-- Default-JavaScript-File --> <script type='text/javascript' src='js/jquery.min.js'></script> <!-- Bootstrap CSS File --> <link href='lib/bootstrap/css/bootstrap.min.css' rel='stylesheet'> <!-- Libraries CSS Files --> <link href='lib/font-awesome/css/font-awesome.min.css' rel='stylesheet'> <link href='lib/animate/animate.min.css' rel='stylesheet'> <link href='lib/ionicons/css/ionicons.min.css' rel='stylesheet'> <link href='lib/owlcarousel/assets/owl.carousel.min.css' rel='stylesheet'> <link href='lib/lightbox/css/lightbox.min.css' rel='stylesheet'><!-- Web-Fonts --><link href='//fonts.googleapis.com/css?family=Raleway:400,500,600,700,800' rel='stylesheet' type='text/css'><link href='//fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'> <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700' rel='stylesheet'><!-- //Web-Fonts --></head><body> <!--========================== Header ============================--> <header id='header'> <div class='container-fluid'> <div id='logo' class='pull-left'> <h1><a href='index' class='scrollto'>Yet Another Trip Advisor</a></h1> <!-- Uncomment below if you prefer to use an image logo --> <!-- <a href='#intro'><img src='img/logo.png' alt='' title='' /></a>--> </div> <nav id='nav-menu-container'> <ul class='nav-menu'> <li class='menu-active'><a href='index.html'>Search</a></li> <li class='menu-has-children'><a href='service.html'>Service</a> <ul> <li><a href='service.html'>Travel</a></li> <li><a href='#hotel'>Hotels</a></li> <li><a href='#flight'>Flights</a></li> </ul> </li> <li><a href='about.html'>About Us</a></li> <li><a href='contact.html'>Contact</a></li> </ul> </nav><!-- #nav-menu-container --> </div> </header><!-- #header --><!--=================Empty Session ============================--><section><div class='top'></div></section><!--========================== Login in/ Register ============================--><section class='loginregister'><div class='container'><div class='login'>");
          String userName;
 
          HttpSession session = request.getSession(false);
          if (session == null) {
-            out.println("<h3>You have not <a href='login.html'>Login</a>!</h3>");
+            out.println("<ul><li><a href='register.html'> Create Account </a></li><li><a href='login.html'>Login</a></li></ul>");
          } else {
             synchronized (session) {
                userName = (String) session.getAttribute("username");
-               out.println("<h3>Hello! <a href='account'>" + userName + "</a></h3>");
-               out.println("<p><a href='logout'>Logout</a></p>");
+               out.println("<ul><li><a href='account'>Hello! " + userName + "</a></li><li><a href='logout'>Logout</a></li></ul>");
             }
           }
+          out.println("</div></div></section><!--========================== Plan Details ============================--><section id = 'plandetail'> <div class = 'container'> <header class='section-header'> <br/><br/> <h3 class='section-title'>Plan Details</h3> </header> <div class='inforpannel'>");
+
          // Step 3: Execute a SQL SELECT query
          String sqlStr = "SELECT * FROM Plan_Date, Plan WHERE itemID = "
                + request.getParameter("itemID") + " AND Plan_Date.planID = Plan.planID";
@@ -46,36 +47,34 @@ public class PlanDetailsServlet extends HttpServlet {  // JDK 6 and above only
          // Print an HTML page as output of query
          ResultSet rset = stmt.executeQuery(sqlStr);
          rset.next();
-         out.println("<h2>" + rset.getString("planTitle") + "</h2>");
+         out.println("<h4 align='center'> PLAN TITLE: " + rset.getString("planTitle") + "</h4>");
             // Print a paragraph <p>...</p> for each row
-         out.println("<p>Starting From: " + rset.getString("startCity") + "</p>");
-         out.println("<p>Date: " + rset.getDate("startDate")+" - "+rset.getDate("endDate")+"</p>");
-         out.println("<p>$"+rset.getInt("price")+"</p>");
-         out.println("<p>"+rset.getInt("remaining_seat")+" seats remaining. </p>");
+         out.println("<p align='center'>starting from: " + rset.getString("startCity"));
+         out.println("<br/>Date: " + rset.getDate("startDate")+" - "+rset.getDate("endDate"));
+         out.println("<br/>price: $"+rset.getInt("price"));
+         out.println("<br/>Remaining Seats: "+rset.getInt("remaining_seat")+"</p>");
+         out.println("</div><img src='images/travel/01.jpg' alt='' class='img-fluid'></img></div></section>");
          out.println("<form method='post' action='checkout'>");
          out.println("<input type='hidden' name='itemID' value=" + rset.getInt("itemID") + " />");
-         out.println("<input type='submit' value='join' />");
+         out.println("<div class = 'button' align='center'><input type='submit' value='Join'></div>");
          out.println("</form>");
-         out.println("<br />");
-         out.println("<br />");
-         out.println("<br />");
-         out.println("<p>===============================</p>");
          String sqlStr2 = "SELECT * FROM Plan_Date WHERE planID = "
                + rset.getInt("planID");
-         out.println("<h3>All available dates for this plan: </h3>");
+         out.println(" <section id = 'available'> <div class = 'container'> <header class='section-header'> <br/><br/> <h3 class='section-title'>ALL AVAILABLE DATES FOR THE PLAN</h3> <br/> <br/> <div class='row hotdeals-cols'> ");
          ResultSet rset2 = stmt.executeQuery(sqlStr2);
          while(rset2.next()){
-
-            out.println("<p>Duration: "+rset2.getDate("startDate")+" - "+rset2.getDate("endDate")+"</p>");
-            out.println("<p>$"+rset2.getInt("price")+"</p>");
-            out.println("<p>"+rset2.getInt("remaining_seat")+" seats remaining. </p>");
+            out.println("<div class='col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp' data-wow-delay='0.1s'> <div class = 'availabledate'> <div class='portfolio-wrap'> <p>");
+            out.println("<br/> Duriation: "+rset2.getDate("startDate")+" - "+rset2.getDate("endDate"));
+            out.println("<br/> price: $"+rset2.getInt("price"));
+            out.println("<br/> Remaining Seats: "+rset2.getInt("remaining_seat")+"</p>");
             out.println("<form method='post' action='detail'>");
             out.println("<input type='hidden' name='itemID' value=" + rset2.getInt("itemID") + " />");
-            out.println("<input type='submit' value='check' />");
+            out.println("<div class = 'button' align='center'><input type='submit' value='Check'></div>");
             out.println("</form>");
+            out.println("</div></div></div>");
          }
 
-         out.println("</body></html>");
+         out.println("</div></div></session><!--========================== Footer ============================--> <div class='footer'> <p> IM2073 Course Project: Yet Another Trip Advisor.<br/>Presented by Xu Peisen and Huang Xinwei <br/><br/></p> </div></body></html>");
       } catch (SQLException ex) {
          ex.printStackTrace();
      } catch (ClassNotFoundException ex) {
